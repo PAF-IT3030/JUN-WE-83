eeffe265de0ca24dd8681414e26870aeed146006
it21310614 <it21310614@my.sliit.lk>
2024-04-18 20:57:37 +0530

    chage workout status card style

--- frontend/src/Components/WorkoutStatusCard/WorkoutStatusCard.jsx
+++ frontend/src/Components/WorkoutStatusCard/WorkoutStatusCard.jsx
@@ -1,42 +1,39 @@
 import React, { useState } from "react";
 import "./WorkoutStatusCard.css";
 import log from "../../Images/avatar.png";
+import wcard from "../../Images/wcard.png";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 
 function WorkoutStatusCard() {
-  const [showOptions, setShowOptions] = useState(false);
-
-  const toggleOptions = () => {
-    setShowOptions(!showOptions);
-  };
-
   return (
     <div className="WorkoutStatusCard">
-      <div className="imgSection">
-        <img src={log} alt="profile-image" />
+      <div className="img-sec">
+        <img src={log} alt="prefile-image" />
       </div>
       <div className="detail-sec">
-        <div className="f-set">
+        <div className="f-sec">
           <p className="userName">Monika</p>
-          <p>@ram 17/04/2024</p>
-          <p>
-            <MoreHorizIcon />
-          </p>
-          {/* {showOptions && (
-            <>
-              <button>Edit</button>
-              <button>Delete</button>
-            </>
-          )} */}
+          <p>@ram . 5m</p>
+          <MoreHorizIcon />
         </div>
-        <div className="s-set">
-          <p className="workoutName">Push Ups 😎✌️</p>
-          <p>Push Ups - 15</p>
-          <p>Did push-ups as part of upper body workout.</p>
+        <div className="s-sec">
+          <p>Today My Status is best 😎✌️</p>
         </div>
         <div className="t-sec">
+          <p className="workoutName">Push Ups</p>
+          <div className="tt-sec">
+            <div className="tt-img">
+              <img src={wcard} alt="wcard" />
+            </div>
+            <div className="td-sec">
+              <h3 className="workoutCount">20 Push Ups</h3>
+              <p>2024.04.18</p>
+            </div>
+          </div>
+        </div>
+        <div className="f-sec">
           <FavoriteBorderIcon />
           <ChatBubbleOutlineIcon />
         </div>
