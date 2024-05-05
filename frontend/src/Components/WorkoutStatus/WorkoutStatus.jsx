eaeb98beac2a411db9460a9222f249b69899ad9e
it21310614 <it21310614@my.sliit.lk>
2024-05-05 22:32:52 +0530

    done create workout function

--- frontend/src/Components/WorkoutStatus/WorkoutStatus.jsx
+++ frontend/src/Components/WorkoutStatus/WorkoutStatus.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React,{useState} from "react";
 import "./WorkoutStatus.css";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function WorkoutStatus() {
 
   const handleBack = () => navigate(-1);
   const navigate = useNavigate();
+
+  const [workoutName, setworkoutName] = useState("");
+  const [workoutDate, setworkoutDate] = useState("");
+  const [workoutMatrix, setworkoutMatrix] = useState("");
+  const [workoutDescription, setworkoutDescription] = useState("");
+
+  const createWorkout=async(e)=>{
+    e.preventDefault();
+
+    try {
+      const response=await axios.post("http://localhost:8087/api/v1/workout/save",{
+        workoutName,
+        workoutDate,
+        workoutMatrix,
+        workoutDescription
+      });
+
+      console.log(response.data);
+      alert("workout created success");
+
+      setworkoutName("");
+      setworkoutDate("");
+      setworkoutMatrix("");
+      setworkoutDescription("");
+      
+    } catch (error) {
+      console.log("error creating workout",error);
+    }
+  }
 
   return (
     <div className="WorkoutStatus">
@@ -23,11 +53,23 @@ function WorkoutStatus() {
     </section>
       <div className="formSection">
         <form className="form-sec">
-          <input type="text" placeholder="Exercise" required />
-          <input type="date" placeholder="Date" required/>
-          <input type="text" placeholder="Matrix" required/>
-          <textarea type="text" placeholder="Description" required/>
-          <button type="submit">CREATE CURRENT STATUS</button>
+          <input type="text" placeholder="Exercise" required 
+          value={workoutName} 
+          onChange={(e)=>{setworkoutName(e.target.value)}}
+          />
+          <input type="date" placeholder="Date" required
+          value={workoutDate}
+          onChange={(e)=>{setworkoutDate(e.target.value)}}
+          />
+          <input type="text" placeholder="Matrix" required
+          value={workoutMatrix}
+          onChange={(e)=>{setworkoutMatrix(e.target.value)}}
+          />
+          <textarea type="text" placeholder="Description" required
+          value={workoutDescription}
+          onChange={(e)=>{setworkoutDescription(e.target.value)}}
+          />
+          <button type="submit" onClick={createWorkout}>CREATE CURRENT STATUS</button>
         </form>
       </div>
     </div>
