f9e9baff087a746ab6f6629fc5aa2b5b7893b731
eranda-est <erandaliyanage100@gmail.com>
2024-05-14 19:38:34 +0530

    Create insert API

---
+++ backend/backend/src/main/java/com/example/backend/Controller/WorkoutPlanController.java
@@ -0,0 +1,26 @@
+package com.example.backend.Controller;
+
+import com.example.backend.Entity.WorkoutPlan;
+import com.example.backend.Service.WorkoutPlanServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/v1/WorkoutPlan")
+public class WorkoutPlanController {
+
+    @Autowired
+    private WorkoutPlanServices workoutPlanServices;
+
+    @PostMapping("/save")
+    public String saveWorkoutPlan(@RequestBody WorkoutPlan workoutPlan) {
+        workoutPlanServices.saveOrUpdate(workoutPlan);
+        return workoutPlan.getId();
+    }
+
+
+
+
+
+}
