9d27948abbfe276be9015b1b4bf7d14ce12bbe0b
it21310614 <it21310614@my.sliit.lk>
2024-05-15 18:18:05 +0530

    add constructor and getters and setters

--- backend/backend/src/main/java/com/example/backend/Controller/UserController.java
+++ backend/backend/src/main/java/com/example/backend/Controller/UserController.java
@@ -1,5 +1,18 @@
 package com.example.backend.Controller;
 
+import com.example.backend.Entity.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("api/v1/user")
 public class UserController {
+
+    @PostMapping(value = "/save")
+    private String saveUser(@RequestBody User users)
+    {
+        return users.id;
+    }
+
 
 }
--- backend/backend/src/main/java/com/example/backend/Entity/User.java
+++ backend/backend/src/main/java/com/example/backend/Entity/User.java
@@ -3,7 +3,7 @@ package com.example.backend.Entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Users")
+@Document(collection = "users")
 public class User {
 
     @Id
@@ -12,5 +12,55 @@ public class User {
     private String email;
     private String password;
 
-    
+    public User(String _id, String password, String email, String name) {
+        this._id = _id;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+    }
+
+    public User() {
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "_id='" + _id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
